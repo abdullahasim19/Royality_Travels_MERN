73cebc3f6de8985e3b5585df1d6815d8e61fe6a6
abdullahasim19 <abdullahasimlhe@gmail.com>
2022-12-17 14:39:27 +0500

    Minor changes fixed

--- Client/src/components/HomePage/Info.js
+++ Client/src/components/HomePage/Info.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 const Info = () => {
+
   return (
-<div className="container mt-3">
-    
-<div className="jumbotron " style={{backgroundColor: 'lightgray'}}>
-  <div className="container">
-    <h1 className="text-center">Who are we?</h1>
-    <p>We aim to become trend-setters in Pakistan. 
-      We give people opportunities to travel to some of the unexplored places in Pakistan alongside
-       usual sight-seeing trips. We have conducted dozens of successful events since and
-        inspired hundreds of people. So what are you waiting for? Select your type of 
-        adventure and get on-board! Bon Voyage!
-    </p>
-    <Link to='/auth' className="btn btn-dark " href=' ' data-toggle="tooltip" title="Click here to Create Account!" role="button">Get Started!</Link>
-   
-  </div>
-</div>    
-</div>
+    <div className="container mt-3">
+
+      <div className="jumbotron " style={{ backgroundColor: 'lightgray' }}>
+        <div className="container">
+          <h1 className="text-center">Who are we?</h1>
+          <p>We aim to become trend-setters in Pakistan.
+            We give people opportunities to travel to some of the unexplored places in Pakistan alongside
+            usual sight-seeing trips. We have conducted dozens of successful events since and
+            inspired hundreds of people. So what are you waiting for? Select your type of
+            adventure and get on-board! Bon Voyage!
+          </p>
+          <Link to='/auth' className="btn btn-dark " data-toggle="tooltip" title="Click here to Create Account!" role="button">Get Started!</Link>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
